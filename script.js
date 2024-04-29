7fa3ad101b2809ed401e3bd2da65c6af4e52774f
Piotr Chleborowicz <p.chleborowicz@gmail.com>
2024-04-29 22:21:19 +0200

    Poprawki komentarzy

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 //Punkt 1,2 i 3
 
-// Definicja funkcji przy użyciu metody map() i funkcji strzałkowej
+//Funkcja, metoda map() i funkcja strzałkowa
 const podwajanieWartosciWTablicy = (tablica) => {
-  // Używamy metody map() do zwrócenia nowej tablicy,
-  // gdzie każda liczba jest podwojona
+  //Metoda map() zwraca tablicę z podwojonymi liczbami
   let podwojoneLiczby = tablica.map((liczba) => liczba * 2);
   return podwojoneLiczby;
 };
 
-// Przykładowe użycie funkcji
+//Przykładowe użycie funkcji
 const daneWejsciowe = [1, 2, 3, 4, 5];
 
-// Wywołujemy funkcję podwojTablice() i przypisujemy wynik do zmiennej
+//Wywołujemy funkcję podwajanieWartosciWTablicy()
 const wynik = podwajanieWartosciWTablicy(daneWejsciowe);
 
-// Wyświetlamy oryginalną tablicę i tablicę podwojoną w konsoli
+//Wyświetlamy tablicę wejściową i wyjściową
 console.log("Tablica wejściowa:", daneWejsciowe);
 console.log("Tablica z podwojonymi wartościami:", wynik);
 
 //Punkt 4,5 i 6
 
-// Definicja funkcji przy użyciu metody map() i funkcji strzałkowej
+//Funkcja, metoda map() i funkcja strzałkowa
 const pobierzImionaStudentow = (studenci) => {
-  // Używamy metody map() do zwrócenia nowej tablicy zawierającej tylko imiona
+  // Zwrócamy tablicę z imionami
   let imionaStudentow = studenci.map((student) => student.imie);
   return imionaStudentow;
 };
 
-// Przykładowa tablica obiektów reprezentujących studentów
+// Tablica studentów
 const studenci = [
   { nr: 1, imie: "Piotr", wiek: 28 },
   { nr: 2, imie: "Adam", wiek: 25 },
@@ -35,9 +34,9 @@ const studenci = [
   { nr: 4, imie: "Rafał", wiek: 31 },
 ];
 
-// Wywołujemy funkcję pobierzImionaStudentow() i przypisujemy wynik do zmiennej
+// Wywołujemy funkcję pobierzImionaStudentow()
 const imionaStudentow = pobierzImionaStudentow(studenci);
 
-// Wyświetlamy oryginalną tablicę obiektów studentów oraz tablicę zawierającą tylko imiona w konsoli
+// Wyświetlamy tablicę wejściową i z imionami
 console.log("Tablica wszystkich danych studentów:", studenci);
 console.log("Tablica z imionami studentów:", imionaStudentow);
